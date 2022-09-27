d0cdbe58e1b9b589980a927e6dcc1dc52c84498e
Jonas Stettner <mail@jstet.net>
2022-09-27 13:26:56 +0200

    first variables work

--- render.py
+++ render.py
@@ -2,27 +2,32 @@
 from jinja2 import Environment, FileSystemLoader
 import subprocess
 from rich.console import Console
+import json
 
 console = Console()
 
-name = "Jonas"
-
-console.log("Starting.")
-
-console.print(f"Hello {name}, this script will now autogenerate your CV.")
-
+json_file = "data.json"
 template_file = "templates/template.tex"
 latex_file = "CV.tex"
 latex_dir = "latex/"
-out_file = "CV.tex"
+out_file = "CV.pdf"
 
-environment = Environment(loader=FileSystemLoader("./"), trim_blocks=True,block_start_string='@@',block_end_string='@@',variable_start_string='@=', variable_end_string='=@')
+console.log("Starting.")
+
+with open('data.json') as f:
+    data = json.load(f)
+
+name = data["name"]
+console.print(f"Hello {name}, this script will now autogenerate your CV.")
+
+
+environment = Environment(loader=FileSystemLoader("./"), trim_blocks=True, block_start_string='@@',block_end_string='@@',variable_start_string='@=', variable_end_string='=@')
 
 template = environment.get_template(template_file)
 
-with open(latex_dir + latex_file, 'w') as fh:
-    fh.write(template.render(
-        name = "Jonas",
+with open(latex_dir + latex_file, 'w') as f:
+    f.write(template.render(
+        data
     ))
     
 
@@ -31,7 +36,7 @@ console.rule("Pandoc doing it's work...")
 p = subprocess.Popen(["pdflatex", latex_file], cwd=latex_dir)
 p.wait()
 
-p = subprocess.Popen(["mv", latex_dir + latex_file, out_file])
+p = subprocess.Popen(["mv", latex_dir + out_file, out_file])
 p.wait()
 
 console.rule()
